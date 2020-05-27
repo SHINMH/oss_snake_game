d6789909690019ebd36b895c38ae79eeb78cbde2
J.H. Park <mail@jonghy.uk>
2020-05-27 13:42:16 +0900

    Comment on youWinScreen, gameOverScreen, startGame, exitYN func. and modify eatFood, refreshInfoBar funtion comments.

--- src/snake.c
+++ src/snake.c
@@ -294,11 +294,10 @@ void move(int snakeXY[][SNAKE_ARRAY_SIZE], int snakeLength, int direction)
 }
 
 /**
-* @brief int eatFood(int snakeXY[][SNAKE_ARRAY_SIZE], int foodXY[]) 
 * 먹이와 접촉했을때, 먹이를 먹을 수 있는지 확인하고 가능하다면 먹는 함수.
 *
-* @param int snakeXY[][SNAKE_ARRAY_SIZE] :
-* @param int foodXY[] : 
+* @param int snakeXY[][SNAKE_ARRAY_SIZE] : Snake의 좌표와 길이
+* @param int foodXY[] : 먹이의 좌표
 * @return 0: 먹이가 존재하지 않음, 1: 정상적으로 먹이를 먹은 경우.
 **/
 int eatFood(int snakeXY[][SNAKE_ARRAY_SIZE], int foodXY[]) 
@@ -559,12 +558,15 @@ void displayHighScores(void) //NEED TO CHECK THIS CODE!!!
 
 //**************END HIGHSCORE STUFF**************//
 
+/**
+* 플레이어가 게임에서 이겼을 경우 'YOU WIN'
+**/
 void youWinScreen(void)
 {
-	int x = 6, y = 7;
-	gotoxy(x,y++);
+	int x = 6, y = 7; // 메시지 시작 좌표 지정 (6,7)
+	gotoxy(x,y++); // x, y 좌표값을 따라 이동, 후위 연산자이므로 (6, 7)로 이동후 y값이 8로 증가됨.
 	printf("'##:::'##::'#######::'##::::'##::::'##:::::'##:'####:'##::: ##:'####:");
-	gotoxy(x,y++);
+	gotoxy(x,y++); // x 값을 유지한 상태로 한 줄 아래로 이동... (이후 반복)
 	printf(". ##:'##::'##.... ##: ##:::: ##:::: ##:'##: ##:. ##:: ###:: ##: ####:");
 	gotoxy(x,y++);
 	printf(":. ####::: ##:::: ##: ##:::: ##:::: ##: ##: ##:: ##:: ####: ##: ####:");
@@ -580,20 +582,23 @@ void youWinScreen(void)
 	printf(":::..::::::.......::::.......:::::::...::...:::....::..::::..::....::");
 	gotoxy(x,y++);	
 	
-	waitForAnyKey();
+	waitForAnyKey(); //사용자 키 입력이 있을때까지 대기.
 	clrscr(); //clear the console
 	return;
 }
 
+/**
+* 게임 오버되었을 때 'GAME OVER' 메시지를 ascii art 형태로 출력.
+**/
 void gameOverScreen(void)
 {
-	int x = 17, y = 3;
+	int x = 17, y = 3; // 메시지 시작 좌표 지정 (17,3)
 	
-	//http://www.network-science.de/ascii/ <- Ascii Art Gen
+	//http://www.network-science.de/ascii/ <- Ascii Art Gen : 아스키 아트 생성 사이트 URL (참조용 주석)
 	
-	gotoxy(x,y++);
+	gotoxy(x,y++); // x, y 좌표값을 따라 이동, 후위 연산자이므로 (17, 3)으로 이동후 y값이 8로 증가됨.
 	printf(":'######::::::'###::::'##::::'##:'########:\n");
-	gotoxy(x,y++);
+	gotoxy(x,y++); // x 값을 유지한 상태로 한 줄 아래로 이동... (이후 반복)
 	printf("'##... ##::::'## ##::: ###::'###: ##.....::\n");
 	gotoxy(x,y++);
 	printf(" ##:::..::::'##:. ##:: ####'####: ##:::::::\n");
@@ -624,65 +629,77 @@ void gameOverScreen(void)
 	gotoxy(x,y++);
 	printf(":.......::::::...:::::........::..:::::..::....::\n");
 	
-	waitForAnyKey();
-	clrscr(); //clear the console
+	waitForAnyKey(); //사용자 키 입력이 있을때까지 대기.
+	clrscr(); //화면을 비워줌
 	return;
 }
 
-//Messy, need to clean this function up
+/**
+* 게임 시작시 호출되는 게임의 메인 로직.
+*
+* @param int snakeXY[][SNAKE_ARRAY_SIZE] : 플레이어의 X, Y 좌표값.
+* @param int foodXY[][] : 먹이의 X, Y 좌표값.
+* @param int consoleWitdh : 창의 넓이
+* @param int consoleHeight : 창의 높이
+* @param int snakeLength : snake의 초기 길이
+* @param int direction : 초기 진행 방향
+* @param int score : 초기 점수값
+* @param int speed : 초기 게임의 속도 (단계)
+**/
+//Todo /maybe-later: 코드 정리 필요.
 void startGame( int snakeXY[][SNAKE_ARRAY_SIZE], int foodXY[], int consoleWidth, int consoleHeight, int snakeLength, int direction, int score, int speed)
 {
-	int gameOver = 0;
-	clock_t endWait;
+	int gameOver = 0; // gameOver 여부 체크 변수. (0: 게임 진행, 1: 게임 오버, 2: 승리)
+	clock_t endWait; // 대기 종료 시간을 담을 변수.
 	
 	//CLOCKS_PER_SEC-(n-1)*(CLOCKS_PER_SEC/10)
-	int waitMili = CLOCKS_PER_SEC-(speed)*(CLOCKS_PER_SEC/10);	//Sets the correct wait time according to the selected speed
-	int tempScore = 10*speed;
-	int oldDirection;
-	int canChangeDirection = 1;
+	int waitMili = CLOCKS_PER_SEC-(speed)*(CLOCKS_PER_SEC/10);	// 현재 게임 속도에 맞는 대기 시간 설정 (대기 시간 : 1초 - 게임속도(단계) * 0.1초)
+	int tempScore = 10*speed; // 속도 증가 시점에서 현재 스코어와 비교할 기준값을 위한 임시 변수. 초기값 : 10 * 속도.
+	int oldDirection = 0; // 직전 방향값을 저장하기 위한 변수
+	int canChangeDirection = 1; // 방향 전환이 가능한 상태인지 저장 (0: 불가능, 1: 가능)
 	//int seconds = 1;
 
-	endWait = clock() + waitMili;
+	endWait = clock() + waitMili; // 현재 시간 + 대기 시간을 대기 종료 시간으로 설정.
 
 	do
 	{
-		if(canChangeDirection)
+		if(canChangeDirection) //방향 전환이 가능한 상태인 경우.
 		{
-			oldDirection = direction;
-			direction = checkKeysPressed(direction);
+			oldDirection = direction; // 현재 방향을 oldDirection에 저장.
+			direction = checkKeysPressed(direction); //새 방향값을 받아 대입.
 		}
 		
-		if(oldDirection != direction)//Temp fix to prevent the snake from colliding with itself
-			canChangeDirection = 0;
+		if(oldDirection != direction) // 직전 방향과 다른 경우
+			canChangeDirection = 0; //방향 전환이 불가능하도록 설정. (snake가 스스로 충돌하는 것을 방지)
 			
-		if(clock() >= endWait) //haha, it moves according to how fast the computer running it is...
+		if(clock() >= endWait) // 대기 종료 시간이 지난 경우. (컴퓨터 속도에 따라 동작.)
 		{
 			//gotoxy(1,1);
 			//printf("%d - %d",clock() , endWait);
-			move(snakeXY, snakeLength, direction);
-			canChangeDirection = 1;
+			move(snakeXY, snakeLength, direction); // Snake를 지정한 방향으로 이동.
+			canChangeDirection = 1;  //다시 방향 전환이 가능하도록.
 
 				
-			if(eatFood(snakeXY, foodXY))
+			if(eatFood(snakeXY, foodXY)) // 현재 좌표에 먹이가 존재한다면
 			{
-				generateFood( foodXY, consoleWidth, consoleHeight, snakeXY, snakeLength); //Generate More Food
-				snakeLength++;
-				score+=speed;
+				generateFood( foodXY, consoleWidth, consoleHeight, snakeXY, snakeLength); //새로운 먹이 생성.
+				snakeLength++; //Snake의 길이 증가.
+				score+=speed; //현재 속도만큼 점수 부여.
 				//x++;
 				//gotoxy(1,1);
 				//printf("%d >= %d", 10*speed+score, tempScore);
-				if( score >= 10*speed+tempScore)
+				if( score >= 10*speed+tempScore) // 현재 점수가 게임 속도 * 10 + 기준값이 되는 현재 단계 스코어보다 큰 경우 게임 속도 증가 처리.
 				//if( 2 >= 2)
 				{
-					speed++;
-					tempScore = score;
+					speed++; // 게임 속도 증가.
+					tempScore = score; // 판단 기준값을 현재 스코어로 변경.
 
-					if(speed <= 9)//this needs to be fixed
-						waitMili = waitMili - (CLOCKS_PER_SEC/10);
-					else
+					if(speed <= 9)//this needs to be fixed // 게임 속도가 9 이하인 경우
+						waitMili = waitMili - (CLOCKS_PER_SEC/10); // 대기 시간 단축 : 기존 대기시간 - 1/10초
+					else // 게임 속도가 9보다 큰 경우
 					{
-						if(waitMili >= 40) //Maximum Speed (the game has to be beatable)
-							waitMili = waitMili - (CLOCKS_PER_SEC/200);
+						if(waitMili >= 40) // 현재 대기 시간이 게임 클럭 속도 기준 40 이상인 경우에만 속도 증가. (그보다 더 빠를 경우 현실적으로 게임 진행 불가한 속도이므로.)
+							waitMili = waitMili - (CLOCKS_PER_SEC/200); // 기존 대기시간 - 1/200초. (속도 증가폭 낮춤)
 						
 					}
 					//level++;
@@ -693,95 +710,43 @@ void startGame( int snakeXY[][SNAKE_ARRAY_SIZE], int foodXY[], int consoleWidth,
 					//x = 0;
 				}
 				
-				refreshInfoBar(score, speed);
+				refreshInfoBar(score, speed); // 하단 바 갱신.
 			}
 			
-			endWait = clock() + waitMili; //TEMP FIX, NEED TO FIND A WAY TO RESET CLOCK().. Na, seems to work fine this way...
+			// HACK : clock() 리셋 방법 찾아서 수정 필요. (현재는 동작하는 것으로 보임)
+			endWait = clock() + waitMili; //현재 시간 기준으로 대기 종료 시간 생신. 
 		}
 		
-		gameOver = collisionDetection(snakeXY, consoleWidth, consoleHeight, snakeLength);
+		gameOver = collisionDetection(snakeXY, consoleWidth, consoleHeight, snakeLength); // 게임 오버 조건 체크해 종료 코드 대입 (Snake가 창 끝에 닿았거나 Snake끼리 닿은 경우 1 대입 = 게임 오버)
 
-		if(snakeLength >= SNAKE_ARRAY_SIZE-5) //Just to make sure it doesn't get longer then the array size & crash
+		if(snakeLength >= SNAKE_ARRAY_SIZE-5) // Snake의 길이가 (설정한 최대 길이 - 5) 이상인 경우 게임 승리. (배열 크기보다 길어질 경우 크래시 발생하므로.)
 		{
-			gameOver = 2;//You Win! <- doesn't seem to work - NEED TO FIX/TEST THIS
-			score+=1500; //When you win you get an extra 1500 points!!!
+			gameOver = 2;//You Win! <- doesn't seem to work - NEED TO FIX/TEST THIS : gameOver에 승리시를 뜻하는 코드 2 삽입. (while문 탈출 유도) => 동작하지 않는다함.
+			score+=1500; //승리시, 보너스 포인트 1500원 지급.
 		}
 		
-	} while (!gameOver);
+	} while (!gameOver); //gameOver값이 0이 아닐때까지 loop.
 	
-	switch(gameOver)
+	switch(gameOver) //gameOver 값에 따라 분기
 	{
-		case 1:
+		case 1: //패배시
 			printf("\7"); //Beep
-			printf("\7"); //Beep
+			printf("\7"); //Beep : 비프음 2회 출력
 
-			gameOverScreen();
+			gameOverScreen(); // 게임 오버 화면 호출후 탈출
 
 			break;
-		case 2:
-			youWinScreen();
+		case 2: //승리시
+			youWinScreen(); // 승리 화면 호출후 탈출
 			break;
 	}
 	
-	if(score >= getLowestScore() && score != 0)
+	if(score >= getLowestScore() && score != 0) //스코어 값이 존재하고 기존 랭킹의 최 하위값보다 스코어가 높은 경우 (= 순위권에 들어온 경우)
 	{
-		inputScore(score);
-		displayHighScores();
+		inputScore(score); // 랭킹 데이터에 현재 스코어 기록.
+		displayHighScores(); // 랭킹 출력.
 	}
 	
-	return;
-}
-
-void loadEnviroment(int consoleWidth, int consoleHeight)//This can be done in a better way... FIX ME!!!! Also i think it doesn't work properly in ubuntu <- Fixed
-{
-	int i;
-	int x = 1, y = 1;
-	int rectangleHeight = consoleHeight - 4;
-	clrscr(); //clear the console
-	
-	gotoxy(x,y); //Top left corner
-	
-	for (; y < rectangleHeight; y++)
-	{
-		gotoxy(x, y); //Left Wall 
-		printf("%c",WALL);
-		
-		gotoxy(consoleWidth, y); //Right Wall
-		printf("%c",WALL);
-	}
-	
-	y = 1;
-	for (; x < consoleWidth+1; x++)
-	{
-		gotoxy(x, y); //Left Wall 
-		printf("%c",WALL);
-		
-		gotoxy(x, rectangleHeight); //Right Wall
-		printf("%c",WALL);
-	}
-	
-/*
-	for (i = 0; i < 80; i++)
-	{
-		printf("%c",WALL);
-	}
-	
-	for (i = 0; i < 17; i++)
-	{
-		printf("%c\n",WALL);
-	}
-
-	for (i = 0; i < 21; i++)
-	{
-		printf("%c\n",WALL);
-		gotoxy(80,i);
-	}
-	
-	for (i = 0; i < 81; i++)
-	{
-		printf("%c",WALL);
-	}	
-*/	
 	return;
 }
 
@@ -974,24 +939,27 @@ void controls(void)
 	return;
 }
 
+/**
+* 종료 화면을 출력하고, 사용자로부터 입력을 받은 값이 y라면 게임을 종료.
+**/
 void exitYN(void)
 {
-	char pressed;
-	gotoxy(9,8);
-	printf("Are you sure you want to exit(Y/N)\n");
+	char pressed; // 사용자의 입력값을 담을 변수. 한 글자로 char 타입. 
+	gotoxy(9,8); // 메시지 출력을 위해 (9,8) 위치로 좌표 이동.
+	printf("Are you sure you want to exit(Y/N)\n"); //종료 여부 확인 메시지.
 	
-	do
+	do //무조건 한번은 실행
 	{
-		pressed = waitForAnyKey();
-		pressed = tolower(pressed);
-	} while (!(pressed == 'y' || pressed == 'n'));
+		pressed = waitForAnyKey(); // waitForAnyKey() Func로부터 키 입력받아 pressed에.
+		pressed = tolower(pressed); // 대문자일 경우 전부 소문자로 변환.
+	} while (!(pressed == 'y' || pressed == 'n')); // y또는 n값이 아닌 경우 계속 반복.
 	
-	if (pressed == 'y')
+	if (pressed == 'y') // y값인 경우 (=사용자가 종료 선택)
 	{
-		clrscr(); //clear the console
-		exit(1);
+		clrscr(); //지금까지 뿌린 화면 전부 삭제후.
+		exit(1); //프로그램 종료. (code 1 : 정상 종료)
 	}
-	return;
+	return; //n인 경우 : 함수 탈출, 기존 작업 계속 수행.
 }
 
 int mainMenu(void)
