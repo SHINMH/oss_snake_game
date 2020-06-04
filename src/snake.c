593cefa54cf758d578dd419ff1e3dacae4b24927
shinsangwook <shinsw6455@naver.com>
2020-06-05 01:43:21 +0900

    Remove Cursor blink
    To remove cursor blink in game, so I add CursorVIew func.
    And, I use this func in move, menuSelector func.

--- src/snake.c
+++ src/snake.c
@@ -335,6 +335,23 @@ void moveSnakeArray(int snakeXY[][SNAKE_ARRAY_SIZE], int snakeLength, int direct
 	return;
 }
 /**
+* 커서 숨기는 함수.
+* ConsoleCursor.bVisible로 커서를 나타내는 것을 컨트롤한다.
+**/
+void CursorView(char show)
+{
+	HANDLE hConsole;
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	ConsoleCursor.bVisible = show; //true : 보임, false : 안보임
+	ConsoleCursor.dwSize = 1; //커서사이즈
+
+	return;
+}
+
+/**
 * 방향키 입력 후 뱀의 모습을 나타내는 함수.
 * 꼬리 부분을 먼저 삭제하고, 이동 전의 머리부분을 몸통으로 바꿈
 * 그 후, moveSnakeArray 함수를 이용해 몸통부분을 바꾸고, 뱀의 머리방향을 설정
@@ -368,8 +385,8 @@ void move(int snakeXY[][SNAKE_ARRAY_SIZE], int snakeLength, int direction)
 	gotoxy(snakeXY[0][0],snakeXY[1][0]);	
 	printf("%c",SNAKE_HEAD);
 	
-	gotoxy(1,1); //(1,1)로 커서 이동
-	
+	//gotoxy(1,1); //(1,1)로 커서 이동
+	CursorView(0); //커서 숨김
 	return;
 }
 
@@ -1008,7 +1025,8 @@ int menuSelector(int x, int y, int yStart)
 	printf(">");
 	
 	//커서 좌표 (1,1) 이동
-	gotoxy(1,1);
+	//gotoxy(1,1);
+	CursorView(0); //커서 숨김
 
 	//방향키 위, 아래를 입력하며 메뉴 선택 구현
 	do
